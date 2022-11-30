d58bcc381bf26a4d0910d09d14c3268af738e5da
Duncan Moyo <duncan@darkpools.co.za>
2022-11-30 15:41:26 +0200

    Navbar and Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
-
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
 
 function App() {
+  const [selectedPage, setselectedPage] = useState("home");
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-  
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setselectedPage={setselectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setselectedPage={setselectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 }
